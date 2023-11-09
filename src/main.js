29c2dcce165da5060f424fa0830ec26d502624a3
clai <2967536544@qq.com>
2023-11-09 20:52:01 +0800

    dist

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-import router from './router';
-import '@/assets/css/common.css';
-import App from './App.vue';
-
-createApp(App).use(router).mount('#app');
--- src/utils/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param {String} selector 选择器
- * @returns {HTMLElement}
- */
-export function getEl(selector) {
-  return document.querySelector(selector);
-}
