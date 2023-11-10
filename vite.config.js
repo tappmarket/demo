d273b256fe9bcbd24bbcb65f1bfa98625f26cdb8
clai <2967536544@qq.com>
2023-11-10 15:49:44 +0800

    feat: compression

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,21 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import ElementPlus from 'unplugin-element-plus/vite';
+import Compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue(), ElementPlus()],
+  plugins: [
+    vue(),
+    ElementPlus(),
+    Compression({
+      threshold: 1024 * 30,
+      deleteOriginFile: false,
+      algorithm: 'gzip',
+      ext: '.gz'
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
