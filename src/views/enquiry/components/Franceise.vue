471f6a3db825f822b0e852fb4c86d743f4525f01
clai <2967536544@qq.com>
2024-01-17 13:44:58 +0800

    feat: form submit

--- src/views/enquiry/components/Franceise.vue
+++ src/views/enquiry/components/Franceise.vue
@@ -1,132 +1,62 @@
 <script setup>
-import { reactive, toRaw } from 'vue';
-import { ElRadio, ElRadioGroup } from 'element-plus';
+import { ref, onBeforeUnmount } from 'vue';
+import { ElButton } from 'element-plus';
 
 defineOptions({
   name: 'EnquiryFranceise'
 });
 const emit = defineEmits(['submit']);
 
-const form = reactive({
-  name: '',
-  email: '',
-  phone: '',
-  storeName: '',
-  claim: '',
-  message: '',
-  employee: 'former',
-  file: ''
-});
-
+const file = ref();
 function uploadFile(e) {
-  const file = e.target.files[0];
+  /** @type {File} */
+  const _file = e.target.files[0];
+  file.value = _file;
 }
 
-// 提交
+const loading = ref(false);
+onBeforeUnmount(() => {
+  loading.value = false;
+});
+
 function onSubmit() {
-  const data = toRaw(form);
-  emit('submit', data);
+  loading.value = true;
+  emit('submit', 'enquiry-franceise');
 }
 </script>
 
 <template>
-  <form class="form" method="POST" action="https://formsubmit.co/cc363f8980a2fcf38e19c31a78d1a019" enctype="multipart/form-data">
-    <input type="hidden" id="formId" name="formId" value="Employee Helpline">
-      <div class="row">
-      <input class="form-inp" name="name" placeholder="Name" v-model="form.name" />
-      <input
-        class="form-inp"
-        type="email"
-        name="email"
-        placeholder="Email"
-        inputmode="email"
-        v-model="form.email"
-      />
-      <input
-        class="form-inp"
-        type="tel"
-        name="tel"
-        placeholder="Phone"
-        inputmode="tel"
-        v-model="form.phone"
-      />
+  <form
+    name="enquiry-franceise"
+    class="form"
+    method="POST"
+    action="https://formsubmit.co/cc363f8980a2fcf38e19c31a78d1a019"
+    enctype="multipart/form-data"
+  >
+    <input type="hidden" id="formId" name="formId" value="Employee Helpline" />
+    <div class="row">
+      <input class="form-inp" name="name" placeholder="Name" />
+      <input class="form-inp" type="email" name="email" placeholder="Email" inputmode="email" />
+      <input class="form-inp" type="tel" name="tel" placeholder="Phone" inputmode="tel" />
     </div>
     <div class="row">
-      <input class="form-inp" name="storeName" placeholder="Store Name" v-model="form.storeName" />
-      <input class="form-inp" name="claim" placeholder="Claim" v-model="form.claim" />
+      <input class="form-inp" name="storeName" placeholder="Store Name" />
+      <input class="form-inp" name="claim" placeholder="Claim" />
     </div>
 
     <div class="row flex-col">
       <span class="label-txt">Attachment</span>
       <label class="inp-label" for="select-file">
         <span class="label-text">Select File</span>
-        <div class="select-file"></div>
-        <input
-          hidden
-          id="select-file"
-          type="file"
-          name="file"
-          placeholder=""
-          @change="uploadFile"
-        />
+        <div class="select-file">{{ file?.name }}</div>
+        <input hidden id="select-file" type="file" name="file" @change="uploadFile" />
       </label>
     </div>
     <div class="row">
-      <textarea
-        class="form-inp message"
-        name="message"
-        placeholder="Message"
-        v-model="form.message"
-      ></textarea>
+      <textarea class="form-inp message" name="message" placeholder="Message"></textarea>
     </div>
-    <button type="submit" class="submit" @click="onSubmit">Send Message</button>
+    <ElButton :loading="loading" class="submit" @click="onSubmit">Send Message</ElButton>
   </form>
 </template>
 
 <style src="./style.scss"></style>
-<style scoped lang="scss">
-:deep(.el-radio-group) {
-  .el-radio {
-    height: 2.67vw;
-    color: #000;
-  }
-
-  .el-radio__inner {
-    --radio-size: 1.87vw;
-    width: var(--radio-size);
-    height: var(--radio-size);
-
-    &::after {
-      --radio-checked-size: 0.53vw;
-      width: var(--radio-checked-size);
-      height: var(--radio-checked-size);
-    }
-  }
-
-  .el-radio__label {
-    font-size: 1.6vw;
-  }
-  .el-radio__input.is-checked + .el-radio__label {
-    color: #000;
-  }
-
-  @include respond-to('phone') {
-    .el-radio {
-      height: 6.67vw;
-    }
-
-    .el-radio__inner {
-      --radio-size: 3.73vw;
-
-      &::after {
-        --radio-checked-size: 1.07vw;
-      }
-    }
-
-    .el-radio__label {
-      font-size: 3.73vw;
-      padding-left: 2.13vw;
-    }
-  }
-}
-</style>
